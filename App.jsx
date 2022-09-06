056064c266e45f333dbdc9582e763781db1a5a89
hirom <1870157+hirom@users.noreply.github.com>
2022-09-06 23:42:57 +0900

    Created LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 //import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 //import MemoListScreen from "./src/screens/MemoListScreen";
-import MemoEditScreen from "./src/screens/MemoEditScreen";
+//import MemoEditScreen from "./src/screens/MemoEditScreen";
 //import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+//import LogInScreen from "./src/screens/LogInScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
 
 export default function App() {
 	//return <MemoListScreen />;
 	// return <MemoDetailScreen />;
-	return <MemoEditScreen />;
+	//return <MemoEditScreen />;
 	//return <MemoCreateScreen />;
+	//return <LogInScreen />;
+	return <SignUpScreen />;
 }
