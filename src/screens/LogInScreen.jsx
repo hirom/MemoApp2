ec79b90c032180ca6105332c1e99f1fc187db223
hirom <1870157+hirom@users.noreply.github.com>
2022-09-09 09:53:22 +0900

    Set TextInput options

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	View,
 	Text,
@@ -11,13 +11,34 @@ import Button from "../components/Button";
 
 export default function LogInScreen(props) {
 	const { navigation } = props;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
 	return (
 		<View style={styles.container}>
 			<View style={styles.inner}>
-				<Text style={styles.title}>Log In</Text>
-				<TextInput style={styles.input} value="Email Address" />
-				<TextInput style={styles.input} value="password" />
+				<Text style={styles.title}>Sign Up</Text>
+				<TextInput
+					style={styles.input}
+					value={email}
+					onChangeText={(text) => {
+						setEmail(text);
+					}}
+					autoCapitalize="none"
+					keyboardType="email-address"
+					placeholder="email"
+					textContentType="emailAddress"
+				/>
+				<TextInput
+					style={styles.input}
+					value={password}
+					onChangeText={(text) => setPassword(text)}
+					autoCapitalize="none"
+					keyboardType="email-address"
+					placeholder="password"
+					secureTextEntry
+					textContentType="password"
+				/>
 				<Button
 					label="Submit"
 					onPress={() => {
@@ -25,13 +46,13 @@ export default function LogInScreen(props) {
 					}}
 				/>
 				<View style={styles.footer}>
-					<Text style={styles.footerText}>Not registered?</Text>
+					<Text style={styles.footerText}>Already registered?</Text>
 					<TouchableOpacity
 						onPress={() => {
-							navigation.reset({ index: 0, routes: [{ name: "SignUp" }] });
+							navigation.reset({ index: 0, routes: [{ name: "LogIn" }] });
 						}}
 					>
-						<Text style={styles.footerLink}>Sign up here!</Text>
+						<Text style={styles.footerLink}>Log in.</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
