c4d6c927311a7d2255932e24b2e587608cd331fb
hirom <1870157+hirom@users.noreply.github.com>
2022-09-16 00:16:22 +0900

    Created Loading Component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -11,22 +11,27 @@ import {
 import firebase from "firebase";
 
 import Button from "../components/Button";
+import Loading from "../components/Loading";
 
 export default function LogInScreen(props) {
 	const { navigation } = props;
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
 			if (user) {
 				navigation.reset({ index: 0, routes: [{ name: "MemoList" }] });
+			} else {
+				setIsLoading(false);
 			}
 		});
 		return unsubscribe;
 	}, []);
 
 	function handlePress() {
+		setIsLoading(true);
 		firebase
 			.auth()
 			.signInWithEmailAndPassword(email, password)
@@ -37,11 +42,15 @@ export default function LogInScreen(props) {
 			})
 			.catch((error) => {
 				Alert.alert(error.code);
+			})
+			.then(() => {
+				setIsLoading(false);
 			});
 	}
 
 	return (
 		<View style={styles.container}>
+			<Loading isLoading={isLoading} />
 			<View style={styles.inner}>
 				<Text style={styles.title}>Log In</Text>
 				<TextInput
