5ce1ff2116c53001f2df5ad47457eff826344c0d
hirom <1870157+hirom@users.noreply.github.com>
2022-09-17 17:38:10 +0900

    Improved UX

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -11,6 +11,7 @@ import {
 import firebase from "firebase";
 
 import Button from "../components/Button";
+import { translateErrors } from "../utils";
 
 export default function SignUpScreen(props) {
 	const { navigation } = props;
@@ -27,8 +28,8 @@ export default function SignUpScreen(props) {
 				navigation.reset({ index: 0, routes: [{ name: "MemoList" }] });
 			})
 			.catch((error) => {
-				console.log(error.code, error.message);
-				Alert.alert(error.code);
+				const errorMsg = translateErrors(error.code);
+				Alert.alert(errorMsg.title, errorMsg.description);
 			});
 	}
 
